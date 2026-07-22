a40666070b3c5518b85d98606aecfae03a843345
itsanwarsu <oanwarsuyitno@gmail.com>
2026-07-22 11:25:46 +0700

    cek api key

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
+console.log({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "ADA" : "TIDAK ADA",
 });
 
 module.exports = cloudinary;
