e279382e03fb9811096e3ae531a1866088786503
anwar suyitno <oanwarsuyitno@gmail.com>
2026-07-24 16:23:57 +0700

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -7,12 +7,6 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Pengecekan singkat saat server start
-console.log("Cloudinary Config Status:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "TIDAK ADA",
-  api_key: process.env.CLOUDINARY_API_KEY || "TIDAK ADA",
-  api_secret: process.env.CLOUDINARY_API_SECRET ? "ADA" : "TIDAK ADA",
-});
 
 module.exports = cloudinary;
 
