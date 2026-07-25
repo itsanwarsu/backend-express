b2430054928437ed5577631f95b797063694c4e5
itsanwarsu <oanwarsuyitno@gmail.com>
2026-07-25 18:00:28 +0700

    update routes

--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -7,7 +7,7 @@ const {
   removeWishlist,
 } = require("../controllers/wishlistController");
 
-const { protect } = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/auth");
 
 router.get("/", protect, getWishlist);
 
