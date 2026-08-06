ee9c5619af6fd985356134f3dc96aa0b35363757
itsanwarsu <oanwarsuyitno@gmail.com>
2026-08-06 17:44:59 +0700

    upgrade routes

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -4,12 +4,13 @@ const router = express.Router();
 const {
   sendMessage,
   getMessages,
+  markAsRead,
 } = require("../controllers/messageController");
 
 const { protect } = require("../middleware/auth");
 
 router.post("/", protect, sendMessage);
 router.get("/:conversationId", protect, getMessages);
-router.patch("/read/:conversationId", protect, messageController.markAsRead);
+router.patch("/read/:conversationId", protect, markAsRead);
 
 module.exports = router;
